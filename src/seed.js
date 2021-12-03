96509c72a97ff2e2cf6d9ff8f196a818eefe13f1
Alexsander Mikhaylov <mihaleksval@gmail.com>
2021-12-04 00:45:05 +0500

    many seeds again

--- src/seed.js
+++ src/seed.js
@@ -12,6 +12,17 @@ export function seedDatabse(firebase) {
       emailAddress: 'mihaleksval@gmail.com',
       dateCreated: Date.now(),
     },
+    {
+      country: 'Sweden',
+      city: 'Stockholm',
+      gender: 'female',
+      phoneNumber: '+71111111111',
+      userId: 'g1yEHRBmhsb5NZs3MC3Kcqf1Zwt1',
+      username: 'Violetta',
+      fullName: 'Bjornsson',
+      emailAddress: 'bj@gmail.com',
+      dateCreated: Date.now(),
+    },
   ];
 
   // eslint-disable-next-line prefer-const
@@ -20,28 +31,33 @@ export function seedDatabse(firebase) {
   }
 
   // eslint-disable-next-line prefer-const
-  for (let i = 1; i <= 20; ++i) {
-    for (let username = 1; username <= 20; ++username) {
-      firebase
-        .firestore()
-        .collection('todos')
-        .add({
-          toDosId: i,
-          userId: '1',
-          titleOfToDo: 'CreateApp',
-          toDosAdditional: [
-            {
-              title: '2nd',
-              toDo: 'Get 2nd todo',
-            },
-            {
-              title: '3d',
-              toDo: 'Get 3d todo',
-            },
-          ],
-          imageSrc: `/images/users/${username}/${i}.jpg`,
-          dateCreated: Date.now(),
-        });
+  for (let i = 1; i <= 10; ++i) {
+    try {
+      for (let username = 1; username <= 10; ++username) {
+        firebase
+          .firestore()
+          .collection('todos')
+          .add({
+            toDosId: i,
+            userId: '1',
+            titleOfToDo: 'CreateApp',
+            toDosAdditional: [
+              {
+                title: '2nd',
+                toDo: 'Get 2nd todo',
+              },
+              {
+                title: '3d',
+                toDo: 'Get 3d todo',
+              },
+            ],
+            imageSrc: `/images/users/${username}/${i}.jpg`,
+            dateCreated: Date.now(),
+          });
+      }
+      console.log('data seed was successful');
+    } catch (error) {
+      console.log(error, ' fetch database was failed');
     }
   }
 }
