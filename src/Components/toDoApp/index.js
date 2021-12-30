0942c0958acb87dba9a6a16584b1a9510b24d313
Alexsander Mikhaylov <mihaleksval@gmail.com>
2021-12-31 02:06:40 +0500

    it's okay all works, almost

--- src/Components/toDoApp/index.js
+++ src/Components/toDoApp/index.js
@@ -1,25 +1,46 @@
+import { useState, useContext, useRef } from 'react';
+
+import FirebaseContext from '../../context/firebaseContext';
+import UserContext from '../../context/user';
+
+import useUser from '../../hooks/user';
 // import CheckUserTodo from './actions/checkUserTodo';
 import FormToDo from './add-to-do.withoutFirebase.testOne';
 
 import HeaderToDo from './header.toDo';
-import RouterToDo from './list.routerToDo';
 import ListOfToDo from './list.toDo';
 
-export default function IndexToDo({
-  user,
-  toDosArray,
-  title,
-  toDo,
-  setTitle,
-  setToDo,
-  displayName,
-  setToDoSArray,
-  createdAt,
-  toDoID,
-  firebaseLib,
-  FieldValue,
-  refTodo,
-}) {
+export default function IndexToDo() {
+  const [toDo, setToDo] = useState('');
+  const [title, setTitle] = useState('');
+  const [userId] = useState();
+  const [createdAt] = useState(Number);
+
+  const [toDosArray, setToDoSArray] = useState([]);
+
+  const { firebaseLib, FieldValue } = useContext(FirebaseContext);
+
+  const refTodo = useRef(null);
+  const {
+    user: { displayName },
+  } = useContext(UserContext);
+
+  const { user } = useUser(displayName?.uid);
+
+  const getRandomNumber = () => {
+    var alphanumeric =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    var twentyEight = 28;
+    for (let i = 0; i < twentyEight; ++i) {
+      result += alphanumeric.charAt(Math.random() * alphanumeric.length);
+    }
+
+    return result;
+  };
+
+  let toDoID = getRandomNumber();
+
   return (
     <div className='container flex mx-auto max-w-screen-sm item-center justify-center'>
       <div className='flex flex-col w-2/2'>
@@ -48,11 +69,16 @@ export default function IndexToDo({
               setTitle={setTitle}
               toDosArray={toDosArray}
               setToDoSArray={setToDoSArray}
+              firebaseLib={firebaseLib}
+              FieldValue={FieldValue}
               displayName={displayName}
+              user={user}
+              refTodo={refTodo}
               createdAt={createdAt}
               toDoID={toDoID}
+              userId={userId}
             />
-            {/* <RouterToDo toDoID={toDoID} title={title} toDosArray={toDosArray} /> */}
+            {/* <CheckUserTodo firebaseLib={firebaseLib} user={user} /> */}
           </div>
         </div>
       </div>
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,41 +1,15 @@
-import React, { useState, useContext, useRef, useEffect } from 'react';
-
-import FirebaseContext from '../context/firebaseContext';
-import useUser from '../hooks/user';
-// import CheckUserTodo from './actions/checkUserTodo';
+import React, { useEffect } from 'react';
 
 import PropTypes from 'prop-types';
 
 import NavBarAndHeader from '../pages/navBar';
 import LoggedInUserContext from '../context/logged-in-user';
+import useUser from '../hooks/user';
 
 import IndexToDo from '../Components/toDoApp/index';
-
 export default function Dashboard({ user: loggedInUser }) {
   const { user, setActiveUser } = useUser(loggedInUser?.uid);
-  const [toDo, setToDo] = useState('');
-  const [title, setTitle] = useState('');
-  const [createdAt] = useState(Number);
 
-  const [toDosArray, setToDoSArray] = useState([]);
-
-  const { firebaseLib, FieldValue } = useContext(FirebaseContext);
-
-  const refTodo = useRef(null);
-
-  const getRandomNumber = () => {
-    var alphanumeric =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var result = '';
-    var twentyEight = 28;
-    for (let i = 0; i < twentyEight; ++i) {
-      result += alphanumeric.charAt(Math.random() * alphanumeric.length);
-    }
-
-    return result;
-  };
-
-  let toDoID = getRandomNumber();
   useEffect(() => {
     document.title = 'ToDoList';
   }, []);
@@ -44,20 +18,7 @@ export default function Dashboard({ user: loggedInUser }) {
     <LoggedInUserContext.Provider value={{ user, setActiveUser }}>
       <NavBarAndHeader />
       <div className='grid grid-cols-1 gap-4 justify-between mx-auto max-w-screen-lg'>
-        <IndexToDo
-          toDo={toDo}
-          setToDo={setToDo}
-          title={title}
-          setTitle={setTitle}
-          toDosArray={toDosArray}
-          setToDoSArray={setToDoSArray}
-          firebaseLib={firebaseLib}
-          FieldValue={FieldValue}
-          user={user}
-          refTodo={refTodo}
-          createdAt={createdAt}
-          toDoID={toDoID}
-        />
+        <IndexToDo />
       </div>
     </LoggedInUserContext.Provider>
   );
