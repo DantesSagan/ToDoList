43e0765e75bd915533982c28da660413de020d5c
DantesSagan <mihaleksval@gmail.com>
2022-03-13 03:02:01 +0500

    remote: changing sign-up css

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -37,7 +37,7 @@ export default function SignUp() {
 
   return (
     <section className='bgLogin'>
-      <div className='container flex mx-auto max-w-screen-sm items-center justify-center 2xl:h-screen xl:h-full lg:h-full md:h-full heightPages arrow-down arrow-up'>
+      <div className='container flex mx-auto max-w-screen-sm items-center justify-center 2xl:h-full xl:h-full lg:h-full md:h-full heightPages arrow-down arrow-up'>
         <div className='flex flex-col 2xl:w-2/4 1xl:w-2/4 xl:w-2/4 lg:w-2/4 md:w-2/4 sm:w-2/4 border-t border-8 border-red-600 greetPages'>
           <div className='flex flex-col items-center bg-white p-4 border border-gray-primary rounded'>
             <h1 className='flex justify-center w-full'>
