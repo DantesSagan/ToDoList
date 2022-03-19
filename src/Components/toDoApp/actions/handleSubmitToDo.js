db5f8d936a48f9024020bf85ddeea6225b22d85f
Alexsander Mikhaylov <mihaleksval@gmail.com>
2022-03-19 15:35:51 +0500

    little changes about time coz not correctly formatted

--- src/Components/toDoApp/actions/handleSubmitToDo.js
+++ src/Components/toDoApp/actions/handleSubmitToDo.js
@@ -49,31 +49,54 @@ export default function HandleSubmitToDo({
         //   return Math.max(Math.random() * (max - min) + min).toFixed(0);
         // }
         // let resultID = getRandomNumber(2000000000000, 5);
-       const formatTime = () => {
-         let date = new Date();
-         // Year part from the timestamp
-         let year = date.getFullYear();
-         // Month part from the timestamp
-         let month =
-           date.getMonth() + 1 === 10 || 11 || 12
-             ? `0${date.getMonth() + 1}`
-             : date.getMonth() + 1;
-         // Days part from the timestamp
-         let days =
-           date.getDate() === 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9
-             ? `0${date.getDate()}`
-             : date.getDate();
-         // Hours part from the timestamp
-         let hours = date.getHours();
-         // Minutes part from the timestamp
-         let minutes = date.getMinutes();
-         // Seconds part from the timestamp
-         let seconds = date.getSeconds();
+        const formatTime = () => {
+          let date = new Date();
+          // Year part from the timestamp
+          let year = date.getFullYear();
+          // Month part from the timestamp
+          let month =
+            date.getMonth() + 1 === 10 || 11 || 12
+              ? `0${date.getMonth() + 1}`
+              : date.getMonth() + 1;
+          // Days part from the timestamp
+          let days =
+            date.getDate() === 10 ||
+            11 ||
+            12 ||
+            13 ||
+            14 ||
+            15 ||
+            16 ||
+            17 ||
+            18 ||
+            19 ||
+            20 ||
+            21 ||
+            22 ||
+            23 ||
+            24 ||
+            25 ||
+            26 ||
+            27 ||
+            28 ||
+            29 ||
+            30 ||
+            31
+              ? `${date.getDate()}`
+              : date.getDate()
+              ? `0${date.getDate()}`
+              : date.getDate();
+          // Hours part from the timestamp
+          let hours = date.getHours();
+          // Minutes part from the timestamp
+          let minutes = date.getMinutes();
+          // Seconds part from the timestamp
+          let seconds = date.getSeconds();
 
-         // Will display time in 10:30:23 format
-         let formattedTime = `Posted time toDo: ${year} year, ${month} month, ${days} day, ${hours}:${minutes}:${seconds}`;
-         return formattedTime;
-       };
+          // Will display time in 10:30:23 format
+          let formattedTime = `Posted time toDo: ${year} year, ${month} month, ${days} day, ${hours}:${minutes}:${seconds}`;
+          return formattedTime;
+        };
         await setDoc(editRef, {
           toDosArray: arrayUnion({
             displayName: displayName,
--- src/Components/toDoApp/list.routerToDo.js
+++ src/Components/toDoApp/list.routerToDo.js
@@ -52,7 +52,30 @@ export default function RouterToDo({
         : date.getMonth() + 1;
     // Days part from the timestamp
     let days =
-      date.getDate() === 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9
+      date.getDate() === 10 ||
+      11 ||
+      12 ||
+      13 ||
+      14 ||
+      15 ||
+      16 ||
+      17 ||
+      18 ||
+      19 ||
+      20 ||
+      21 ||
+      22 ||
+      23 ||
+      24 ||
+      25 ||
+      26 ||
+      27 ||
+      28 ||
+      29 ||
+      30 ||
+      31
+        ? `${date.getDate()}`
+        : date.getDate()
         ? `0${date.getDate()}`
         : date.getDate();
 
