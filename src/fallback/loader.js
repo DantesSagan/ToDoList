d0463af7aa54407d244d62ad78780e65dbf8eeed
Alexsander Mikhaylov <mihaleksval@gmail.com>
2022-03-30 23:58:51 +0500

    updating react to 18.0.0 and changing loader component

--- src/fallback/loader.js
+++ src/fallback/loader.js
@@ -1,26 +1,22 @@
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import Loader from 'react-loader-spinner';
-import styled from 'styled-components';
+import React from 'react';
 
-const Fallback = () => (
-  <FallbackContainer>
-    <Loader
-      type='ThreeDots'
-      color='black'
-      height={80}
-      width={80}
-      timeout={3000}
-    />
-  </FallbackContainer>
-);
-
-export default Fallback;
-
-const FallbackContainer = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%) rotate(0.5turn);
-  background: red;
-  border-radius: 40px;
-`;
+export default function Loader() {
+  return (
+    <div className='text-1xl text-center rounded-lg mx-auto greetPages h-screen'>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        className='h-36 w-36 spinnerLoader mx-auto mt-72'
+        viewBox='0 0 20 20'
+        fill='black'
+        stroke='red'
+      >
+        <path
+          fill-rule='evenodd'
+          d='M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z'
+          clip-rule='evenodd'
+        />
+      </svg>
+      <span className='font-bold p-4'>You're awesome!</span>
+    </div>
+  );
+}
